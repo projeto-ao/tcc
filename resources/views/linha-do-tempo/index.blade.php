781272d2c594ae186bbbe6635ade68f2cbb58e63
ghp2201 <ghp2201@gmail.com>
2020-06-04 14:25:35 -0300

    Criado script de template para postagem
    Criado um script de template para a postagem, aind autilizando o modelo
    herdado do antigo Orbis.

--- resources/views/linha-do-tempo/index.blade.php
+++ resources/views/linha-do-tempo/index.blade.php
@@ -3,12 +3,34 @@
 @section('title', 'Projeto AO')
 
 @section('css')
+<link type="text/css" rel="stylesheet" href="{{ asset('public/css/estilos.css') }}">
 @endsection
 
 @section('content')
-    <div class="corpo">
+    <div class="container">
+        <div class="col-md-12 col-xs-10">
 
+        <div>
     </div>
+
+    <script type="text/template" id="template_postagem">
+        <div class = "postagem">
+            <div class = "nome-usuario">
+            </div>
+
+            <div class = "fonte-corpo">
+            </div>
+
+            <div>
+                <a class = "fonte-botoes-corpo">
+                    <i class="fab fa-pagelines"></i> Gostei
+                </a>
+                <a class = "fonte-botoes-corpo">
+                    <i class="fas fa-bezier-curve"></i> Compartilhar
+                </a>
+            </div>
+        </div>
+    </script>
 @endsection
 
 @section('js')
