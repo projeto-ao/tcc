76469ce921633ecc7d77a429d2a5a41ee3362e4f
ghp2201 <ghp2201@gmail.com>
2020-07-06 12:16:13 -0300

    Renomeado estilos.css para importador.css
    Renomeado o arquivo central de estilos.css para importador.css afim de
    se evitar confusoes acerca da sua funcionalidade.

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.0/css/mdb.min.css" rel="stylesheet">
-        <link type="text/css" rel="stylesheet" href="{{ asset('public/css/estilos.css') }}">
+        <link type="text/css" rel="stylesheet" href="{{ asset('public/css/importador.css') }}">
 
         @yield('css')
     </head>
