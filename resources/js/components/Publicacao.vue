b421cf30dd26a9ea30447643bb8615829c015aad
ghp2201 <ghp2201@gmail.com>
2020-09-13 19:16:50 -0300

    Melhorados estilos em mobile

--- resources/js/components/Publicacao.vue
+++ resources/js/components/Publicacao.vue
@@ -50,34 +50,37 @@
             </button>
         </div>
     </form>
+    <!---->
 
-    <div class="row col-md-8 offset-md-2"
+    <div class="col-12 col-md-8 offset-md-2"
         v-show="!comentarios"
     >
-        <button class="col-md btn btn-elegant botao-interacao" title="Curtir"
-            @click="curtir(id)"
-        >
-            <span v-show="curtidas" :id="'curtidas-' + id">
-                {{ curtidas }}
-            </span>
-            <i class="fab fa-pagelines"></i>
-        </button>
-        <a class="col-md btn btn-elegant botao-interacao" title="Comentários"
-            :href="'./comentarios/' + id"
-        >
-            <span v-show="qtdComentarios" :id="'comentarios-' + id">
-                {{ qtdComentarios }}
-            </span>
-            <i class="fas fa-signature"></i>
-        </a>
-        <button class="col-md btn btn-elegant botao-interacao" title="Compartilhar"
-            @click="compartilhar(id)"
-        >
-            <span v-show="compartilhamentos" :id="'compartilhamentos-' + id">
-                {{ compartilhamentos }}
-            </span>
-            <i class="fas fa-share-alt"></i>
-        </button>
+        <div class="row">
+            <button class="col btn btn-elegant botao-interacao" title="Curtir"
+                @click="curtir(id)"
+            >
+                <span v-show="curtidas" :id="'curtidas-' + id">
+                    {{ curtidas }}
+                </span>
+                <i class="fab fa-pagelines"></i>
+            </button>
+            <a class="col btn btn-elegant botao-interacao" title="Comentários"
+                :href="'./comentarios/' + id"
+            >
+                <span v-show="qtdComentarios" :id="'comentarios-' + id">
+                    {{ qtdComentarios }}
+                </span>
+                <i class="fas fa-signature"></i>
+            </a>
+            <button class="col btn btn-elegant botao-interacao" title="Compartilhar"
+                @click="compartilhar(id)"
+            >
+                <span v-show="compartilhamentos" :id="'compartilhamentos-' + id">
+                    {{ compartilhamentos }}
+                </span>
+                <i class="fas fa-share-alt"></i>
+            </button>
+        </div>
     </div>
 </div>
 </template>
