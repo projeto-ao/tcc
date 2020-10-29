fdaaf30ea89d9f56ef08bce69edd43cdf24454d2
Gustavo Henrique Pinto <ghp2201@gmail.com>
2020-10-29 18:49:13 -0300

    Adicionado foto do usuario na pagina de perfil

--- resources/views/perfis/index.blade.php
+++ resources/views/perfis/index.blade.php
@@ -11,11 +11,31 @@
   .corpo-publicacao {
     padding: 1rem;
   }
+
+  .corpo-imagem-perfil {
+    border-radius: 50%;
+    background-color: #fff;
+    padding: 1rem;
+  }
+
+  .imagem-perfil {
+    border-radius: 50%;
+    height: 9rem;
+    width: 9rem;
+  }
 </style>
 @endsection
 
 @section('conteudo')
-<div class="col-md-8 offset-md-2">
+<div class="col-6 offset-3 col-md-2 offset-md-5 corpo-imagem-perfil text-center">
+  @if ($perfil->imagem != null)
+    <img class="imagem-perfil" src="{{ asset('public/uploads/' . $perfil->imagem . '') }}"/>
+  @else
+    <img class="imagem-perfil" src="{{ asset('public/imagens/foto_perfil_padrao.jpeg') }}"/>
+  @endif
+</div>
+
+<div class="col-md-6 offset-md-3 text-center">
   <div class="corpo-publicacao">
     <h2>{{ $perfil->name }}</h2>
   </div>
